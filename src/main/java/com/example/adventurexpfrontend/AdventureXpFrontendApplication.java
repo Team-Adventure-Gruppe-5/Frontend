1c63b2453cd71d5aa070d277bffa2f62df1ac6a4
CamillaEva <caha0007@stud.kea.dk>
2025-10-01 11:50:14 +0200

    first commit - spring project

---
+++ src/main/java/com/example/adventurexpfrontend/AdventureXpFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.adventurexpfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureXpFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureXpFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/adventurexpfrontend/AdventureXpFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adventurexpfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureXpFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
