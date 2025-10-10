0a0633dbe7296c82272821d263ad8df4d636d15c
laerkeloenborg <lalo0001@stud.kea.dk>
2025-10-10 09:33:28 +0200

    Customer page

--- js/login.js
+++ js/login.js
@@ -27,27 +27,4 @@ document.getElementById("loginForm").addEventListener("submit", event => {
         });
 });
 
-//customer login
-document.addEventListener('DOMContentLoaded',() =>{
-    const customerForm = document.getElementById("loginCustomerForm")
-    customerForm.addEventListener("submit", ()=>{
-        event.preventDefault();
-        const mail = document.getElementById("mail").value
-        const bookingId = document.getElementById("bookingId").value
 
-        fetch("http://localhost:8080/login-customer", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body:JSON.stringify({mail, bookingId})
-        })
-            .then(response => {
-                if(!response.ok) throw new Error("Wrong credentials")
-                return response.json()
-            })
-            .then(customer => {
-                sessionStorage.setItem("customer", JSON.stringify(customer))
-            })
-            .catch(error => console.log("ERROR", error))
-
-    })
-})
